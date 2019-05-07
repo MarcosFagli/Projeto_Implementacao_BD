17ac53d65ab751913a87f53f490931032c1df8a7
MarcosFagli <marcosfagli@hotmail.com>
2019-05-07 01:02:55 -0300

    Organização dos dados

--- dados/contagem_natal.py
+++ dados/contagem_natal.py
@@ -1,31 +1,60 @@
 import pandas as pd
 
-base_dados = pd.read_excel('Planilha_Primeiro_Dia_de_MercadoFinal.xlsm', sheet_name='Base de dados', usecols="D:E")
+class product:
+    nome = ''
+    tipoUnidade = ''
+    valorUnidade = 0.0
+    codBarras = ''
 
-cb = []
-for i in base_dados['Topo']:
-    cb.append(str(i).strip())
+def detectarMedidaeUnidade(string):
+    listUnit = ["LATA", 'KG', 'UN', "ML", "L", "G"]
 
-item = []
-for i in base_dados['Unnamed: 1']:
-	item.append(i)
+    for i in listUnit:
+        if(i in string[len(string)-5:]):
+            indice = string.find(i, len(string)-5)
+
+            aux = 1
+
+            while(string[indice-aux] in ['0','1','2','3','4','5','6','7','8','9','.',',',' ']):
+                aux += 1
+
+            return(float(string[indice-aux+1:indice].replace(',','.')), i, string[:indice-aux+1])
+
+    return (-1, '', string)
+
+def main():
+    base_dados = pd.read_excel('Planilha_Primeiro_Dia_de_MercadoFinal.xlsm', sheet_name='Base de dados', usecols="D:E")
+
+    cb = []
+    for i in base_dados['Topo']:
+        cb.append(str(i).strip())
+
+    item = []
+    count = 2
+    produtos = base_dados['Unnamed: 4']
+    for j in produtos[2:]:
+        j = j.upper()
+        j = j.replace("(","")
+        j = j.replace(")","")
+
+        retorno = detectarMedidaeUnidade(j)
+
+        temp =[]
+        temp.append(retorno[2])
+        temp.append(retorno[1])
+        temp.append(retorno[0])
+        temp.append(cb[count])
+        count += 1
+
+        item.append(temp)
+
+    file = open('DadosNormalizados.txt', 'w')
+    for i in item:
+        file.write(i[0] + "," + i[1] + "," + str(i[2]) + "," + i[3] + "\n")
+    file.close
+
+    print("Thanks =)")
 
 
-b = list(zip(cb, item))[3:]
-
-a = {}
-for x in b:
-	if x[1] not in a.keys():
-		a[x[1]] = [x[0]]
-	else:
-		a[x[1]].append(x[0])
-
-for y, w in a.items():
-	print(y, w)
-
-print(len(a.values()))
-
-
-
-
-
+if __name__ == "__main__":
+    main()
